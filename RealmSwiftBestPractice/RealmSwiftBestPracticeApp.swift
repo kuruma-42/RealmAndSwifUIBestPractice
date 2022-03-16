efae006cb1960ae97b0c96fd2f560f09dc037217
yongjuncha <perfect_yon@naver.com>
2022-03-16 19:09:12 +0900

    Initial Commit

---
+++ RealmSwiftBestPractice/RealmSwiftBestPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RealmSwiftBestPracticeApp.swift
+//  RealmSwiftBestPractice
+//
+//  Created by Yong Jun Cha on 2022/03/16.
+//
+
+import SwiftUI
+
+@main
+struct RealmSwiftBestPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
